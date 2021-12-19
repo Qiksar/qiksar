a5b177de3dbaf50cde6d0abfd33e4e926038d9e6
chris nurse <chris.nurse@namsource.com>
2021-12-19 12:18:00 +0800

    End to end build

--- docker/initkc/kc_init.sh
+++ docker/initkc/kc_init.sh
@@ -13,45 +13,24 @@ echo
 echo "Install JSON Query..."
 microdnf install jq > /dev/null
 
-export PATH=$PATH:/opt/jboss/keycloak/bin
-export OUTPUT_PATH=/docker-entrypoint-initdb.d
-
-export KCSRV=http://localhost:8080
-export DOMAIN=sndk8.net
-
-export KCREALM_ADMIN=admin
-export KCPASS=keycloak
-
-export REALM_JSON=${OUTPUT_PATH}/realm.json
-export REALM_ID=sndk8
-export REALM_NAME=Sndk8 App
-export CLIENT=${REALM_ID}-app
-
-export REALM_ADMIN=adm@${DOMAIN}
-export REALM_ADMIN_ROLE=tenant_admin
-export USER_PW=password
-
-export REALM_USER=usr@${DOMAIN}
-export DEFAULT_ROLE=member
-
-export SMTP_PASSWORD=member
-export SMTP_SENDER_NAME=sender@${DOMAIN}
+PATH=$PATH:/opt/jboss/keycloak/bin
+OUTPUT_PATH=/docker-entrypoint-initdb.d
 
 echo
 echo "Login to Keycloak..."
 
 # Login
-kcadm.sh config credentials --server "${KCSRV}/auth" --realm master --user ${KCREALM_ADMIN} --password ${KCPASS} --client admin-cli
+kcadm.sh config credentials --server "${KCSRV}/auth" --realm master --user ${KEYCLOAK_USER} --password ${KEYCLOAK_PASSWORD} --client admin-cli
 
 # Generate realm template
-cat $OUTPUT_PATH/template_realm.json | sed s/{{realm_id}}/${REALM_ID}/ | sed s/{{REALM_NAME}}/$2/ | sed s/{{SMTP_PASSWORD}}/$3/ | sed s/{{SMTP_SENDER_NAME}}/$4/  > ${OUTPUT_PATH}/realm.json
+cat $OUTPUT_PATH/template_realm.json | sed "s|{{REALM_ID}}|${REALM_ID}|" | sed "s|{{REALM_NAME}}|${REALM_NAME}|" | sed "s|{{SMTP_PASSWORD}}|${SMTP_PASSWORD}|" | sed "s|{{SMTP_SENDER_NAME}}|${SMTP_SENDER_NAME}|"  > ${OUTPUT_PATH}/realm.json
 
 # Create the realm
 echo
 echo "Create the realm: "${REALM_ID}
 kcadm.sh create realms -s realm=${REALM_ID} -s enabled=true
 kcadm.sh create partialImport -r ${REALM_ID} -s ifResourceExists=SKIP -f ${REALM_JSON}
-kcadm.sh create roles -r ${REALM_ID} -s name=${REALM_ADMIN_ROLE} -s description='Tenant admin can manage users within the realm'
+kcadm.sh create roles -r ${REALM_ID} -s name=${APP_ADMIN_ROLE} -s description='Tenant admin can manage users within the realm'
 
 # Set the default role assigned to all users
 echo
@@ -59,18 +38,18 @@ echo "Create the default role for the realm: "${DEFAULT_ROLE}
 kcadm.sh add-roles    -r ${REALM_ID} --rname default-roles-${REALM_ID} --rolename ${DEFAULT_ROLE}
 
 echo
-echo "Create tenant admin user: "${REALM_ADMIN}" with password: "${USER_PW}
-kcadm.sh create users -r ${REALM_ID} -s username=${REALM_ADMIN} -s enabled=true
-kcadm.sh set-password -r ${REALM_ID} --username=${REALM_ADMIN} --new-password ${USER_PW}
-echo "Assign admin and tenant_admin roles: "${REALM_ADMIN}" with password: "${USER_PW}
-kcadm.sh add-roles    -r ${REALM_ID} --uusername ${REALM_ADMIN} --rolename "admin" 
-kcadm.sh add-roles    -r ${REALM_ID} --uusername ${REALM_ADMIN} --rolename ${REALM_ADMIN_ROLE}  
+echo "Create tenant admin user: "${APP_ADMIN}" with password: "${USER_PW}
+kcadm.sh create users -r ${REALM_ID} -s username=${APP_ADMIN} -s enabled=true
+kcadm.sh set-password -r ${REALM_ID} --username=${APP_ADMIN} --new-password ${USER_PW}
+echo "Assign admin and tenant_admin roles: "${APP_ADMIN}" with password: "${USER_PW}
+kcadm.sh add-roles    -r ${REALM_ID} --uusername ${APP_ADMIN} --rolename "admin" 
+kcadm.sh add-roles    -r ${REALM_ID} --uusername ${APP_ADMIN} --rolename ${APP_ADMIN_ROLE}  
 
 echo
-echo "Create realm test user: "${REALM_USER}" with password: "${USER_PW}
-kcadm.sh create users -r ${REALM_ID} -s username=${REALM_USER} -s enabled=true
-kcadm.sh set-password -r ${REALM_ID} --username=${REALM_USER} --new-password ${USER_PW}
-kcadm.sh add-roles    -r ${REALM_ID} --uusername ${REALM_USER} --rolename ${DEFAULT_ROLE}  
+echo "Create realm test user: "${APP_USER}" with password: "${USER_PW}
+kcadm.sh create users -r ${REALM_ID} -s username=${APP_USER} -s enabled=true
+kcadm.sh set-password -r ${REALM_ID} --username=${APP_USER} --new-password ${USER_PW}
+kcadm.sh add-roles    -r ${REALM_ID} --uusername ${APP_USER} --rolename ${DEFAULT_ROLE}  
 
 echo
 echo "Capture the realm public key..."
@@ -83,8 +62,8 @@ echo ${KC_KEY} > ${OUTPUT_PATH}/private_data/token.env
 echo "Captured the realm public key into:"${OUTPUT_PATH}"/private_data/token.env"
 
 # Test login with user credentials
-echo 'Test authentication - request token for: '${REALM_ADMIN}
-export AUTH_TOKEN=$(curl -s --request POST --url ${KCSRV}/auth/realms/${REALM_ID}/protocol/openid-connect/token   --header 'Content-Type: application/x-www-form-urlencoded'   --data username=${REALM_ADMIN}   --data password=${REALM_ADMIN_PW}   --data grant_type=password   --data client_id=${CLIENT}  | jq -r -c .access_token | sed -e 's/"//g')
+echo 'Test authentication - request token for: '${APP_ADMIN}
+export AUTH_TOKEN=$(curl -s --request POST --url ${KCSRV}/auth/realms/${REALM_ID}/protocol/openid-connect/token   --header 'Content-Type: application/x-www-form-urlencoded'   --data username=${APP_ADMIN}   --data password=${USER_PW}   --data grant_type=password   --data client_id=${CLIENT}  | jq -r -c .access_token | sed -e 's/"//g')
 echo "Bearer "${AUTH_TOKEN} > ${OUTPUT_PATH}/private_data/auth_token.json
 
 echo "Bearer token captured into:"${OUTPUT_PATH}"/private_data/auth_token.json"
--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,31 +1,58 @@
+clear
+
+# Setup all the environment variables in the env file
+echo "Import environment variables from 'local.env'"
+export $(cat local.env | xargs)
+echo
+
+# Create the config file for the Hasura
+echo "Create Hasura console config"
+cat hasura/hasura-migrations/config.template | sed "s|{{HASURA_GRAPHQL_CONSOLE}}|$HASURA_GRAPHQL_CONSOLE|" | sed "s|{{HASURA_GRAPHQL_ADMIN_SECRET}}|$HASURA_GRAPHQL_ADMIN_SECRET|" > hasura/hasura-migrations/config.yaml
+echo
+
 # Pull the latest qiktrak utility container
 # This will enable us to create a database, seed it with data and track all the tables and relationships, with zero-touch
+echo "Pull qiktrak docker image"
 docker pull namsource/qiktrak:latest
+echo
 
 # Tear down and rebuild the docker containers for the database and authentication
 # then give the containers some time to settle
+echo "Stop containers"
 docker compose down
+echo
+
+echo "Start database and authentication containers"
 docker compose up db auth -d
+
+echo "Wait for containers to stabilise"
 sleep 15
+echo
 
 # Initialise the keycloak environment
 # This will create a realm for the app, and create an admin and none admin user
+echo "Configure Keycloak"
 docker exec -u 0 -it q_auth bash -c "chmod a+x /docker-entrypoint-initdb.d/kc_init.sh; /docker-entrypoint-initdb.d/kc_init.sh"
+echo
 
 # Start the graphql container
 # then give it time to settle
+echo "Start Hasura GraphQL container"
 docker compose up gql -d
 sleep 5
+echo
 
 # Start the qiktrak container which will configure Hasura GraphQL
 # The configuration is done according to the contents of the docker/qiktrak folder 
+echo "Execute qiktrak..."
 docker compose up qiktrak 
+echo
 
-# Start the hasura metadata console
-# then give it time to settle
-cd hasura/hasura-migrations
-hasura console &
-sleep 5
-
-# Apply the metadata 
-hasura metadata apply
+echo "Applying metadata doesn't seem to work from this shell script"
+echo "Execute the following commands to apply the Hasura metadata"
+echo
+echo "cd hasura/hasura-migrations"
+echo "hasura metadata apply"
+echo
+echo "Build script complete"
+echo
