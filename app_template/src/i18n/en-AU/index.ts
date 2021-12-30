17b3f2b3fafa3437e9408132810b49aa7df6d46c
Chris Nurse <chris.nurse@namsource.com>
2021-12-31 07:46:07 +0800

    Add i18n config

---
+++ app_template/src/i18n/en-AU/index.ts
@@ -0,0 +1,11 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  failed: 'Ouch cobber',
+  success: 'Fair dinkum, it worked!',
+  leader: 'Boss',
+  member: 'Pleb',
+  State: 'State/Territory',
+  Group: 'Group'
+};
