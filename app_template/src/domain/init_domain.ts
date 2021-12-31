c5b1b6059389898dc46bc69462e5d16e68d9ed36
Chris Nurse <chris.nurse@namsource.com>
2021-12-31 09:48:41 +0800

    add todo

--- app_template/src/domain/init_domain.ts
+++ app_template/src/domain/init_domain.ts
@@ -1,5 +1,7 @@
 export default async function InitialiseDomain(path:string): Promise<void>{
 	
+	// TODO CN: 
+	// Can we make views auto-discoverable so we don't have to remember to add them to this array?
 	const views = ['RolesView', 'StatusView', 'GroupsView', 'MembersView'];
 	
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
