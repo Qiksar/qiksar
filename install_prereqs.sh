33a017da4d6157bd566861b413925a2407c33c31
Chris Nurse <chris.nurse@namsource.com>
2022-01-03 13:50:21 +0800

    Make it obvious whats happening

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -5,6 +5,9 @@ echo "1. You must have NODE installed"
 echo
 echo "2. You must have DOCKER installed"
 echo
-echo "installing yarn, quasar and cordova..."
+echo "installing yarn, you may need to provide your password for the sudo command"
 sudo npm install --global yarn 
+
+echo
+echo "installing quasar and cordova..."
 yarn global add @quasar/cli cordova
