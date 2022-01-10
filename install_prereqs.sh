fe52f93977a19a13ce2424ea1cf447c8dcc1778e
Qiksar Community <qiksar.apps@gmail.com>
2022-01-10 15:44:56 +0800

    Add icon genie

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -5,5 +5,5 @@ echo "1. You must have NODE installed"
 echo
 echo "2. You must have DOCKER installed"
 echo
-echo "Using npm to install yarn, quasar CLI and cordova, your password may be required for sudo..."
-sudo npm i -g yarn @quasar/cli cordova
+echo "Using npm to install yarn, quasar CLI, icon genie, and cordova, your password may be required for sudo..."
+sudo npm i -g yarn @quasar/cli @quasar/icongenie cordova
