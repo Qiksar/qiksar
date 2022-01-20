8fe6512b99cb2ca312452cc39753a7bd982978f4
Qiksar Community <qiksar.apps@gmail.com>
2022-01-20 20:15:55 +0800

    SQL Formatting

--- services/hasura/hasura-migrations/migrations/default/0000000003_create_json_views/up.sql
+++ services/hasura/hasura-migrations/migrations/default/0000000003_create_json_views/up.sql
@@ -2,9 +2,9 @@ CREATE VIEW "membership"."iot_environment" AS
 SELECT
     message_id,
     membership.iot_messages.created_at,
-    CAST(iot_messages.json_data - > > 'temp' AS FLOAT) AS "temperature",
-    CAST(iot_messages.json_data - > > 'humidity' AS INTEGER) AS "humidity_percentage",
-    CAST(iot_messages.json_data - > > 'dust' AS INTEGER) AS "dust_ppm"
+    CAST(iot_messages.json_data->> 'temp' AS FLOAT) AS "temperature",
+    CAST(iot_messages.json_data->> 'humidity' AS INTEGER) AS "humidity_percentage",
+    CAST(iot_messages.json_data->> 'dust' AS INTEGER) AS "dust_ppm"
 FROM
     membership.iot_messages
     INNER JOIN membership.iot_devices
@@ -23,10 +23,10 @@ CREATE VIEW "membership"."iot_smart_watch" AS
 SELECT
     message_id,
     membership.iot_messages.created_at,
-    CAST(iot_messages.json_data - > > 'temp' AS FLOAT) AS "temperature",
-    CAST(iot_messages.json_data - > > 'heart_rate' AS INTEGER) AS "heart_bpm",
-    CAST(iot_messages.json_data - > > 'steps' AS INTEGER) AS "step_count",
-    CAST(iot_messages.json_data - > > 'oxygen' AS INTEGER) AS "O2Sat"
+    CAST(iot_messages.json_data->> 'temp' AS FLOAT) AS "temperature",
+    CAST(iot_messages.json_data->> 'heart_rate' AS INTEGER) AS "heart_bpm",
+    CAST(iot_messages.json_data->> 'steps' AS INTEGER) AS "step_count",
+    CAST(iot_messages.json_data->> 'oxygen' AS INTEGER) AS "O2Sat"
 FROM
     membership.iot_messages
     INNER JOIN membership.iot_devices
