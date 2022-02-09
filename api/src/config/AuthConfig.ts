7f2dc530b6b39589cbd25d18d2e74ffa612d7220
Qiksar Community <qiksar.apps@gmail.com>
2022-02-09 10:00:23 +0000

    Restructure

---
+++ api/src/config/AuthConfig.ts
@@ -0,0 +1,14 @@
+import { KeycloakConnectOptions, TokenValidation } from 'nest-keycloak-connect';
+
+const keycloak_config: KeycloakConnectOptions = {
+  authServerUrl: process.env.KEYCLOAK_ENDPOINT,
+  realm: process.env.KEYCLOAK_REALM,
+  realmPublicKey: process.env.KEYCLOAK_REALM_KEY,
+  clientId: process.env.KEYCLOAK_APICLIENT,
+  secret: process.env.KEYCLOAK_APICLIENT_SECRET,
+  bearerOnly: true,
+  tokenValidation: TokenValidation.ONLINE,
+  useNestLogger: true,
+};
+
+export default keycloak_config;
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
